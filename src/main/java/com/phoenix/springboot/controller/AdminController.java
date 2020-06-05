c850257790e48988f172f21da42644793d2054b7
zhuyuanlv <zhuyuanlv>
2020-06-05 22:43:50 +0800

    删除无用引用

--- src/main/java/com/phoenix/springboot/controller/AdminController.java
+++ src/main/java/com/phoenix/springboot/controller/AdminController.java
@@ -1,6 +1,5 @@
 package com.phoenix.springboot.controller;
 
-import com.phoenix.springboot.mapper.AdminMapper;
 import com.phoenix.springboot.pojo.Admin;
 import com.phoenix.springboot.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +11,11 @@ import java.util.List;
 @RestController
 public class AdminController {
 
-    /*@Autowired
-    private AdminMapper adminMapper;*/
-
     @Autowired
     private AdminService adminService;
 
     @GetMapping("/queryAdminList")
     public List<Admin> queryAdminList(){
-       /* List<Admin> adminList =adminMapper.queryAdminList();
-        return adminList;*/
         return adminService.queryAdminList();
     }
 }
